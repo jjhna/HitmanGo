0f6d20cd6842d2c29217355ebf10b942c3fd09e8
Jon <22411356+jjhna@users.noreply.github.com>
2021-08-04 20:37:02 -0400

    Updated the scene and assets
    Updated the scene and assets

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    // uniform distance between nodes
+    public static float spacing = 2f;
+
+    // four compass directions
+    public static readonly Vector2[] directions =
+    {
+        new Vector2(spacing, 0f),
+        new Vector2(-spacing, 0f),
+        new Vector2(0f, spacing),
+        new Vector2(0f, -spacing)
+    };
+
+    List<Node> m_allNodes = new List<Node>();
+    public List<Node> AllNodes { get { return m_allNodes; } }
+
+    void Awake()
+    {
+        GetNodeList();
+    }
+
+    public void GetNodeList()
+    {
+        Node[] nList = GameObject.FindObjectsOfType<Node>();
+        m_allNodes = new List<Node>(nList);
+    }
+
+}
---
+++ Assets/Scripts/Link.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Link : MonoBehaviour
+{
+    public float borderWidth = 0.02f;
+    public float lineThickness = 0.5f;
+    public float scaleTime = 1f;
+    public float delay = 0.1f;
+    
+    public iTween.EaseType easeType = iTween.EaseType.easeInOutSine;
+
+    public void DrawLink(Vector3 startPos, Vector3 endPos) {
+        transform.localScale = new Vector3(lineThickness, 1f, 0f);
+
+        Vector3 dirVector = endPos - startPos;
+
+        float zScale = dirVector.magnitude - borderWidth * 2f;
+
+        Vector3 newScale = new Vector3(lineThickness, 1f, zScale);
+
+        transform.rotation = Quaternion.LookRotation(dirVector);
+
+        transform.position = startPos + (transform.forward * borderWidth);
+
+        iTween.ScaleTo(gameObject, iTween.Hash(
+            "time", scaleTime,
+            "scale", Vector3.one,
+            "easetype", easeType,
+            "delay", delay
+        ));
+    }
+}
